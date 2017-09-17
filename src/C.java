6c98dbf924c7e2f93a15620a0f2483e63666b63c
156278 <gc11092015@gmail.com>
2017-09-17 17:43:35 +0200

    added diagonal sum class

---
+++ src/C.java
@@ -0,0 +1,43 @@
+import com.sun.xml.internal.fastinfoset.util.StringArray;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class C {
+    public static void diagSum() {
+        Scanner scin4 = new Scanner(System.in);
+        int a = 0, b = 0, c = 0;
+        System.out.println("Please define size of matrix: ");
+        a = scin4.nextInt();
+        int[][] matr = new int[a][a];
+        System.out.println("Please input values followed by enter: ");
+        for (int i = 0; i < a; i++) {
+            for (int j = 0; j < a; j++) {
+                matr[i][j] = scin4.nextInt();
+                FreshScreen.freshScreen();
+                System.out.println("Please input values followed by enter: ");
+                for (int k = 0; k < a; k++) {
+                    for (int l = 0; l < a; l++) {
+                        System.out.printf("%d,", matr[k][l]);
+                    }
+                    System.out.printf("\n");
+                }
+            }
+        }
+
+        for (int i = 0; i < a; i++) {
+            b += matr[i][i];
+
+        }
+        System.out.println();
+        for (int i = 0; i < a; i++) {
+            c += matr[i][a - i - 1];
+
+        }
+        System.out.println("\nThe sum of diagonal numbers minus the sum of the other diagonal numbers equals: " + (b - c)+ "\n");
+
+    }
+
+}
+
--- src/Main.java
+++ src/Main.java
@@ -9,33 +9,37 @@ public class Main {
 
 
     public static void main(String[] args) {
-        System.out.println("This is the menu to my homework:\nPlease choose which part (1-4) you want to load:\n");
-        Scanner scin1 = new Scanner(System.in);
-        int a = scin1.nextInt();
-        System.out.printf("Loading " + a);
-        for (int i = 0; i < 3; i++) {
-            System.out.printf(".");
-            try {
-                Thread.sleep(300);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
+        int a = 1;
+        while (a != 0) {
+            System.out.println("************************************************************************************\nThis is the menu to my homework:\n\n1. Adding & Stats\n2. Staircase\n3. Calculation of diagonal sums in matrices\n4. Time converter\n0. Exit\n\n************************************************************************************\n\nPlease choose which part (0-4) you want to load:\n");
+            Scanner scin1 = new Scanner(System.in);
+            a = scin1.nextInt();
+            System.out.printf("Loading " + a);
+            for (int i = 0; i < 3; i++) {
+                System.out.printf(".");
+                try {
+                    Thread.sleep(300);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
             }
+            FreshScreen.freshScreen();
+            switch (a) {
+                case 1:
+                    A.addAndStats();
+                    break;
+                case 2:
+                    B.staircase();
+                    break;
+                case 3:
+                    C.diagSum();
+                    break;
+                case 4:
+                    break;
+
+
+            }
+
         }
-        FreshScreen.freshScreen();
-        switch (a) {
-            case 1:
-                A.addAndStats();
-                break;
-            case 2:
-                B.staircase();
-                break;
-            case 3:
-                break;
-            case 4:
-                break;
-
-
-        }
-
     }
-}   
+}
